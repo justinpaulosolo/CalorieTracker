1224e56f2ddd03ba34097399ad24d7eab59bab9b
Justin Solo <justinpaulosolo@gmail.com>
2023-10-23 16:36:45 -0700

    fix: removed empty line

--- CalorieTracker.Server/Meals/MealsApi.cs
+++ CalorieTracker.Server/Meals/MealsApi.cs
@@ -12,7 +12,6 @@ public static class MealsApi
         group.WithTags("Meals");
         group.MapPost("/", async (ApplicationDbContext context, CreateMealRequest mealRequest) =>
         {
-
             if (!MiniValidator.TryValidate(mealRequest, out var errors))
             {
                 return Results.ValidationProblem(errors);
