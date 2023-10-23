345588f099fcad4c484a5db89c6af210008bbe91
Justin Solo <justinpaulosolo@gmail.com>
2023-10-23 15:11:06 -0700

    chore: weather api for testing auth

--- CalorieTracker.Server/Program.cs
+++ CalorieTracker.Server/Program.cs
@@ -34,6 +34,32 @@ app.MapAccount();
 
 app.MapGet("/requires-auth", (ClaimsPrincipal user) => $"Hello, {user.Identity?.Name}!").RequireAuthorization();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .RequireAuthorization()
+    .WithOpenApi();
+
 app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
