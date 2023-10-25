f1e81b3ea910f685ded91c77b6bc33327f6491d3
Justin Solo <justinpaulosolo@gmail.com>
2023-10-24 18:20:57 -0700

    feat: email validation for register user

--- CalorieTracker.Server/Account/ApplicationUser.cs
+++ CalorieTracker.Server/Account/ApplicationUser.cs
@@ -12,6 +12,7 @@ namespace CalorieTracker.Server.Users
         [Required]
         public string Username { get; set; } = default!;
         [Required]
+        [EmailAddress]
         public string Email { get; set; } = default!;
         [Required]
         public string Password { get; set; } = default!;
