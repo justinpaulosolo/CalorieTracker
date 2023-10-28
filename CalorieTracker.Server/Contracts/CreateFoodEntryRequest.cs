a98d5936bfa416a3c7e4d6c4ce9a542db42dcd20
Justin Solo <justinpaulosolo@gmail.com>
2023-10-28 13:57:42 -0700

    chore: delete unused files

--- CalorieTracker.Server/Contracts/CreateFoodEntryRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CalorieTracker.Server.Contracts;
-
-public class CreateFoodEntryRequest
-{
-    public int MealId { get; set; }
-    public int FoodId { get; set; }
-}
--- CalorieTracker.Server/Contracts/CreateMealRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CalorieTracker.Server.Contracts;
-
-public class CreateMealRequest
-{
-    public string Name { get; set; } = string.Empty;
-    public string UserId { get; set; } = string.Empty;
-    public DateTime Date { get; set; }
-}
