8ac2b059811ec95a324937a650eb515e3b136480
justinpaulosolo <justinpaulosolo@gmail.com>
2023-11-18 14:37:10 -0800

    refactor: Refactor CreateMealEntryEndpoint to CreateMeal

--- CalorieTracker.Server/Features/Meals/Commands/CreateMealEntry.cs
+++ CalorieTracker.Server/Features/Meals/Commands/CreateMealEntry.cs
@@ -5,94 +5,99 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace CalorieTracker.Server.Features.Meals.Commands;
-public static class CreateMealEntryEndpoint
-{ 
+
+public static class CreateMeal
+{
     public static void MapCreateMealEntryEndpoint(this IEndpointRouteBuilder app)
     {
-        app.MapPost("api/meals", async (CreateMealEntryCommand command, ISender sender, ClaimsPrincipal user) =>
+        app.MapPost("api/meals", async (Command command, ISender sender, ClaimsPrincipal user) =>
         {
             var userId = user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
             command.UserId = userId;
             var foodEntryId = await sender.Send(command);
         }).WithTags("Meals").RequireAuthorization();
     }
-}
 
-public sealed class CreateMealEntryCommand : IRequest<int>
-{
-    public string UserId { get; set; } = null!;
-    public string MealType { get; init; } = null!;
-    public DateTime Date { get; init; }
-    public string Name { get; init; } = null!;
-    public int Proteins { get; init; }
-    public int Carbs { get; init; }
-    public int Fats { get; init; }
-    public int Calories { get; init; }
-    public int Quantity { get; init; }
-}
-
-public class CreateMealEntryHandler
-    (ApplicationDbContext dbContext) : IRequestHandler<CreateMealEntryCommand, int>
-{
-    public async Task<int> Handle(CreateMealEntryCommand command, CancellationToken cancellationToken)
+    public sealed class Command : IRequest<int>
     {
-        var meal = await GetOrCreateMeal(command, cancellationToken);
-        var food = await GetOrCreateFood(command, cancellationToken);
-
-        var foodEntry = new MealFoodEntry
-        {
-            MealId = meal.Id, FoodId = food.Id, Quantity = command.Quantity
-        };
-
-        dbContext.MealFoodEntries.Add(foodEntry);
-        await dbContext.SaveChangesAsync(cancellationToken);
-
-        return foodEntry.Id;
+        public string UserId { get; set; } = null!;
+        public string MealType { get; init; } = null!;
+        public DateTime Date { get; init; }
+        public string Name { get; init; } = null!;
+        public int Proteins { get; init; }
+        public int Carbs { get; init; }
+        public int Fats { get; init; }
+        public int Calories { get; init; }
+        public int Quantity { get; init; }
     }
 
-    private async Task<UserMeal> GetOrCreateMeal(CreateMealEntryCommand command, CancellationToken cancellationToken)
+    public class Handler
+        (ApplicationDbContext dbContext) : IRequestHandler<Command, int>
     {
-        var existingMeal = await dbContext.UserMeals.Where(m =>
-                m.UserId == command.UserId && m.Date.Date == command.Date.Date && m.MealType == command.MealType)
-            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
-
-        if (existingMeal != null)
+        public async Task<int> Handle(Command command, CancellationToken cancellationToken)
         {
-            return existingMeal;
+            var meal = await GetOrCreateMeal(command, cancellationToken);
+            var food = await GetOrCreateFood(command, cancellationToken);
+
+            var foodEntry = new MealFoodEntry
+            {
+                MealId = meal.Id,
+                FoodId = food.Id,
+                Quantity = command.Quantity
+            };
+
+            dbContext.MealFoodEntries.Add(foodEntry);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return foodEntry.Id;
         }
 
-        var meal = new UserMeal
+        private async Task<UserMeal> GetOrCreateMeal(Command command, CancellationToken cancellationToken)
         {
-            UserId = command.UserId, MealType = command.MealType, Date = command.Date
-        };
+            var existingMeal = await dbContext.UserMeals.Where(m =>
+                    m.UserId == command.UserId && m.Date.Date == command.Date.Date && m.MealType == command.MealType)
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken);
 
-        dbContext.UserMeals.Add(meal);
-        await dbContext.SaveChangesAsync(cancellationToken);
-        return meal;
-    }
+            if (existingMeal != null)
+            {
+                return existingMeal;
+            }
 
-    private async Task<FoodItem> GetOrCreateFood(CreateMealEntryCommand command, CancellationToken cancellationToken)
-    {
-        var existingFood = await dbContext.FoodItems.FirstOrDefaultAsync(f =>
-            f.Name == command.Name && f.Proteins == command.Proteins && f.Carbs == command.Carbs &&
-            f.Fats == command.Fats && f.Calories == command.Calories, cancellationToken: cancellationToken);
+            var meal = new UserMeal
+            {
+                UserId = command.UserId,
+                MealType = command.MealType,
+                Date = command.Date
+            };
 
-        if (existingFood != null)
-        {
-            return existingFood;
+            dbContext.UserMeals.Add(meal);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return meal;
         }
 
-        var food = new FoodItem
+        private async Task<FoodItem> GetOrCreateFood(Command command, CancellationToken cancellationToken)
         {
-            Name = command.Name,
-            Proteins = command.Proteins,
-            Carbs = command.Carbs,
-            Fats = command.Fats,
-            Calories = command.Calories
-        };
+            var existingFood = await dbContext.FoodItems.FirstOrDefaultAsync(f =>
+                f.Name == command.Name && f.Proteins == command.Proteins && f.Carbs == command.Carbs &&
+                f.Fats == command.Fats && f.Calories == command.Calories, cancellationToken: cancellationToken);
 
-        dbContext.FoodItems.Add(food);
-        await dbContext.SaveChangesAsync(cancellationToken);
-        return food;
+            if (existingFood != null)
+            {
+                return existingFood;
+            }
+
+            var food = new FoodItem
+            {
+                Name = command.Name,
+                Proteins = command.Proteins,
+                Carbs = command.Carbs,
+                Fats = command.Fats,
+                Calories = command.Calories
+            };
+
+            dbContext.FoodItems.Add(food);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return food;
+        }
     }
 }
