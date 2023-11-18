fe15a5531e67231a43f27284eeed964ae7c06695
justinpaulosolo <justinpaulosolo@gmail.com>
2023-11-18 13:44:05 -0800

    feat: Update route path for meal food entry editing

--- calorietracker.client/src/App.tsx
+++ calorietracker.client/src/App.tsx
@@ -15,7 +15,10 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route element={<PrivateRoute />}>
         <Route path="/" element={<AppPage />} />
-        <Route path="/food-entry/edit/:id" element={<EditFoodEntryPage />} />
+        <Route
+          path="/meal-food-entry/edit/:id"
+          element={<EditFoodEntryPage />}
+        />
         <Route path="/settings" element={<SettingsLayout />}>
           <Route path="/settings/goals" element={<GoalSettingsPage />} />
           <Route path="/settings/account" element={<AccountSettingsPage />} />
