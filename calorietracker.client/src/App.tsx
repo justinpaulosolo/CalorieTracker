be74d31d557aa201042971fa1bfd8d312bf0ac22
justinpaulosolo <justinpaulosolo@gmail.com>
2023-11-29 05:05:34 -0800

    refactor: Update food entry page to edit meal entry

--- calorietracker.client/src/App.tsx
+++ calorietracker.client/src/App.tsx
@@ -6,7 +6,7 @@ import PrivateRoute from "./components/private-route";
 import SettingsLayout from "./components/settings/settings-layout";
 import AccountSettingsPage from "./pages/settings/AccountSettingsPage";
 import GoalSettingsPage from "./pages/settings/GoalSettingsPage";
-import EditFoodEntryPage from "./pages/EditFoodEntryPage";
+import EditMealEntryPage from "./pages/EditMealEntryPage";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route element={<PrivateRoute />}>
         <Route path="/" element={<AppPage />} />
-        <Route path="/food-entry/edit/:id" element={<EditFoodEntryPage />} />
+        <Route path="/food-entry/edit/:id" element={<EditMealEntryPage />} />
         <Route path="/settings" element={<SettingsLayout />}>
           <Route path="/settings/goals" element={<GoalSettingsPage />} />
           <Route path="/settings/account" element={<AccountSettingsPage />} />
