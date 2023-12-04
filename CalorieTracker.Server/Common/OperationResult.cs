0f2eed28721fb589108e8c7f9f6604712db08a55
justinpaulosolo <justinpaulosolo@gmail.com>
2023-12-03 17:28:44 -0800

    refactor: removed unused class

--- CalorieTracker.Server/Common/OperationResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CalorieTracker.Server.Common;
-
-public class OperationResult<T>
-{
-    public T Result { get; init; }
-    public List<string> Errors { get; init; } = new List<string>();
-    public bool IsSuccessful => Errors.Count == 0;
-}
