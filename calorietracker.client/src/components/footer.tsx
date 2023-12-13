3fc44c9c54562c29548b40d8cca4ac87bfb7599f
justinpaulosolo <justinpaulosolo@gmail.com>
2023-12-13 11:41:50 -0800

    feat: footer

--- calorietracker.client/src/components/footer.tsx
+++ calorietracker.client/src/components/footer.tsx
@@ -1,7 +1,16 @@
+import { Banana } from "lucide-react";
+
 export default function Footer() {
   return (
-    <footer className="border-t">
-      <p>Footer</p>
+    <footer className="border-t w-full py-5">
+      <div className="container max-w-5xl flex flex-col">
+        <div className="flex space-x-2 mx-auto">
+          <Banana size={22} />
+          <span className="font-medium">Crispy Happiness</span>
+        </div>
+        <p className="mt-5 text-center text-sm leading-6 text-slate-500">© 2023 Crispy Happiness. All rights
+          reserved.</p>
+      </div>
     </footer>
   );
 };
