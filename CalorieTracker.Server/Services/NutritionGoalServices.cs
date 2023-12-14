51c9178d6c0370d3ea9c7be60d2fb3310a5776b0
justinpaulosolo <justinpaulosolo@gmail.com>
2023-12-14 13:02:23 -0800

    feat: implement `CreateNutritionGoalAsync` and `GetNutritionGoalAsync`

--- CalorieTracker.Server/Services/NutritionGoalServices.cs
+++ CalorieTracker.Server/Services/NutritionGoalServices.cs
@@ -1,16 +1,42 @@
+using CalorieTracker.Server.Data;
+using CalorieTracker.Server.Entities;
 using CalorieTracker.Server.Models.NutritionGoal;
+using Microsoft.EntityFrameworkCore;
 
 namespace CalorieTracker.Server.Services;
 
-public class NutritionGoalServices : INutritionGoalServices
+public class NutritionGoalServices(ApplicationDbContext dbContext) : INutritionGoalServices
 {
     public async Task<int> CreateNutritionGoalAsync(CreateNutritionGoalDto createNutritionGoalDto, string userId)
     {
-        throw new NotImplementedException();
+        var nutritionGoal = new NutritionGoal
+        {
+            Calories = createNutritionGoalDto.Calories,
+            Protein = createNutritionGoalDto.Protein,
+            Carbs = createNutritionGoalDto.Carbs,
+            Fat = createNutritionGoalDto.Fat,
+            UserId = userId
+        };
+        
+        await dbContext.NutritionGoals.AddAsync(nutritionGoal);
+        await dbContext.SaveChangesAsync();
+        return nutritionGoal.NutritionGoalId;
     }
 
     public async Task<NutritionGoalDto?> GetNutritionGoalAsync(string userId)
     {
-        throw new NotImplementedException();
+        var nutritionGoal = await dbContext.NutritionGoals
+            .Where(ng => ng.UserId == userId)
+            .Select(ng => new NutritionGoalDto
+            {
+                NutritionGoalId = ng.NutritionGoalId,
+                Calories = ng.Calories,
+                Protein = ng.Protein,
+                Carbs = ng.Carbs,
+                Fat = ng.Fat
+            })
+            .FirstOrDefaultAsync();
+
+        return nutritionGoal;
     }
 }
