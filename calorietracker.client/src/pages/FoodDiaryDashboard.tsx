cfab2ec742677d814093a1451f4038be6246950e
justinpaulosolo <justinpaulosolo@gmail.com>
2023-12-14 17:52:32 -0800

    feat: display callout if user have not yet set a nutrition goal

--- calorietracker.client/src/pages/FoodDiaryDashboard.tsx
+++ calorietracker.client/src/pages/FoodDiaryDashboard.tsx
@@ -4,13 +4,31 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import NutritionInfoCard from "@/components/nutrition-info-card.tsx";
 import RecentFoodEntriesCard from "@/components/recent-food-entries-card.tsx";
 import NewFoodDiaryEntryPage from "@/pages/NewFoodDiaryEntryPage.tsx";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator.tsx";
+import { useGetNutritionGoal } from "@/hooks/nutrition-goal/useGetNutritionGoal.ts";
 
 function FoodDiaryDashboard() {
+  const nutritionGoal = useGetNutritionGoal();
   const [date] = useState(new Date());
   return (
     <div className="flex flex-col space-y-4">
       <div className="flex justify-between">
         <h1 className="font-bold text-2xl">Food Diary Dashboard</h1>
+        {
+          nutritionGoal.data == undefined && (
+            <Link
+              to="/settings/nutritional-goal"
+              className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
+            >
+              ❗<Separator className="mx-2 h-4" orientation="vertical" />{" "}
+              <span>
+            Set your daily calorie goal in{" "}
+          </span>
+              <ArrowRightIcon className="ml-1 h-4 w-4" />
+            </Link>
+          )
+        }
       </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <NutritionInfoCard date={date} />
