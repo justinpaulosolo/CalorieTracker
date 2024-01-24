55829061a401f2af7feffb3f676435e2213b9da4
Justin Solo <justinpaulosolo@gmail.com>
2024-01-24 14:08:29 -0800

    feat(wip): Account settings page

--- calorietracker.client/src/pages/settings/AccountSettingsPage.tsx
+++ calorietracker.client/src/pages/settings/AccountSettingsPage.tsx
@@ -1,8 +1,80 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useNavigate } from "react-router-dom";
+import { LoginUser } from "@/utils/types.ts";
+import { loginFormSchema } from "@/utils/schemas.ts";
+import { useLoginUser } from "@/hooks/useLoginUser.ts";
+
+//TODO: Make account settings endpoint
 export default function AccountSettingsPage() {
+  const loginUser = useLoginUser();
+  const navigate = useNavigate();
+
+  const form = useForm<LoginUser>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    }
+  });
+
+  function onSubmit(values: LoginUser) {
+    // Todo: Show some error when login fails
+    loginUser.mutate(values, {
+      onSuccess: () => navigate("/"),
+      onError: error => {
+        const formError = {
+          type: "server",
+          message: "Invalid username or password"
+        };
+
+        form.setError("username", formError);
+        form.setError("password", formError);
+        console.log(error);
+      }
+    });
+  }
+
+  // Todo: Fix login forms styling
   return (
-    <div>
-      <h1>Account Settings Page</h1>
-      <h1>Test</h1>
-    </div>
+    <Form {...form}>
+      <form
+        className="flex flex-col space-y-2"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input required {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input required type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button disabled={!form.formState.isValid} type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 }
