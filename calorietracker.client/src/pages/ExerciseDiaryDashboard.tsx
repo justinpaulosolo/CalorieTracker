aa0031fce8f90c34185ee15b0e5f77c0c88eca5f
justinpaulosolo <justinpaulosolo@gmail.com>
2024-02-12 14:42:51 -0800

    feat(wip): exercise dashboard

--- calorietracker.client/src/pages/ExerciseDiaryDashboard.tsx
+++ calorietracker.client/src/pages/ExerciseDiaryDashboard.tsx
@@ -1,7 +1,40 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import NewExerciseEntryPage from "./NewExerciseEntryPage";
+import { useState } from "react";
+import RecentFoodEntriesCard from "@/components/recent-food-entries-card";
+
 function ExerciseDiaryDashboard () {
+  const [date] = useState(new Date());
   return (
     <div className="flex flex-col space-y-4">
       <h1 className="font-bold text-2xl">Exercise Diary Dashboard</h1>
+      <div className="grid gap-4 grid-cols-6 items-start">
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Add Exercise Entry</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <NewExerciseEntryPage date={date} />
+          </CardContent>
+        </Card>
+        <Card className="col-span-3 p-0">
+          <CardHeader>
+            <CardTitle>Recent Food Diary Entries</CardTitle>
+          </CardHeader>
+          <CardContent className="p-3">
+            <RecentFoodEntriesCard date={date} />
+          </CardContent>
+          <CardFooter className="mt-auto">
+            <Link
+              to="/food-diary/detailed"
+              className="text-sm underline underline-offset-4"
+            >
+              View detailed food diary
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   )
 }
---
+++ calorietracker.client/src/pages/NewExerciseEntryPage.tsx
@@ -0,0 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs.tsx";
+import FoodDiaryEntryQuickAddForm from "../components/food-diary/forms/food-diary-entry-quick-add-form.tsx";
+import FoodDiaryEntryAddForm from "@/components/food-diary/forms/food-diary-entry-add-form.tsx";
+
+export default function NewExerciseEntryPage({ date }: { date: Date }) {
+  return (
+    <div className="flex justify-center">
+      <Tabs defaultValue="quickadd" className="w-[500px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="quickadd">Quick Add</TabsTrigger>
+          <TabsTrigger value="yourfoods">Your Exercise</TabsTrigger>
+        </TabsList>
+        <TabsContent value="quickadd" className="mt-4">
+          <FoodDiaryEntryQuickAddForm date={date} />
+        </TabsContent>
+        <TabsContent value="yourfoods">
+          <FoodDiaryEntryAddForm date={date} />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
